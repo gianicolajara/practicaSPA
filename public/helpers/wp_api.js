6c81e79c7e75a166049b6f8f1892dde845921cba
gianicola <gianicolajara@gmail.com>
2021-01-16 10:31:42 -0400

    Insert variables in wp_api, PER_PAGE(const) and page(let) because i need to control the pagination

--- public/helpers/wp_api.js
+++ public/helpers/wp_api.js
@@ -1,11 +1,14 @@
 const WEBSITE = "css-tricks",
+  PER_PAGE = 10,
   DOMAIN = `https://${WEBSITE}.com`,
   WP_JSON = `${DOMAIN}/wp-json`,
   API_WP = `${WP_JSON}/wp/v2`,
-  POST_EMBED = `${API_WP}/posts?_embed`,
+  POST_EMBED = `${API_WP}/posts?_embed&per_page=${PER_PAGE}`,
   POST = `${API_WP}/posts`,
-  SEARCH = `${API_WP}/search?_embed&search=`,
+  SEARCH = `${API_WP}/search?_embed&per_page=${PER_PAGE}&search=`,
   POST_SLUG = `${POST_EMBED}&slug=`;
+
+let page = 1;
 
 export default {
   WEBSITE,
@@ -16,4 +19,5 @@ export default {
   POST,
   SEARCH,
   POST_SLUG,
+  page,
 };
